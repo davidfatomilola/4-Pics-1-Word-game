fe3a36d0d47a1d0b83b6a515e86e5451d0847459
davidfatomilola <97130074+davidfatomilola@users.noreply.github.com>
2022-01-06 04:21:09 +0100

    Update github starter.py

--- github starter.py
+++ github starter.py
@@ -1,2 +1,4 @@
 "let's gooo time to get started on this project"
 "everyone add anything to make sure we understand how to use github"
+"we are gonna work together as a team to achieve this project, let us all cooperate we can do it!"
+"that is all i wanna add for now"
